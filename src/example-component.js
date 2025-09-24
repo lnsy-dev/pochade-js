564daf93432f49655c0a684b6e90f8793a355d35
LNSY <lnsy.dev@proton.me>
2025-09-23 17:28:16 -0700

    added web worker demo

--- src/example-component.js
+++ src/example-component.js
@@ -4,6 +4,15 @@ class ExampleComponent extends DataroomElement {
   async initialize(){
     this.create("h1", {content: "Example Code"});
     this.create("p", {content: "This element uses the dataroom.js. It provides a few features that make using custom HTML Elements easier!"})
+
+    const worker = new Worker(new URL('./example-webworker.js', import.meta.url));
+
+    worker.onmessage = (event) => {
+      console.log("Message received from worker:", event.data);
+      this.create("p", {content: JSON.stringify(event.data)});
+    };
+
+    worker.postMessage({ message: "Hello from the main thread!" });
   }
 }
 
